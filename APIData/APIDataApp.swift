3b093b3ee317ae8cbc24be2f999286b18aa1614c
Apoorva Kanekal <apka8010@colorado.edu>
2023-10-05 22:00:22 -0600

    Initial Commit

---
+++ APIData/APIDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIDataApp.swift
+//  APIData
+//
+//  Created by Apoorva Kanekal on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct APIDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
